804e1d49a81a8602444da0967afefe5e4c83d8d2
canabeast <canabeast@yandex.ru>
2016-07-07 01:28:36 +0300

    class Rename

---
+++ src/lesson002/Part4VisYear.java
@@ -0,0 +1,19 @@
+package lesson002;
+
+import java.util.Scanner;
+
+public class Part4VisYear {
+	public static void main(String[] args) {
+		Scanner comprYear = new Scanner(System.in);
+		System.out.print("Введите год для проверки: ");
+		if (comprYear.hasNextInt()) {
+			int y=comprYear.nextInt();
+			if ((y % 4 != 0)&&(y % 1000 == 0)) {
+				System.out.print(y + " - год не високостный");
+			} else {
+				System.out.print(y + " - год високостный");
+			}
+		} else
+			System.out.print("Год введен не корректно");
+	}
+}
