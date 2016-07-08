2e0f4d3b95e2aa93fb6561c8e5e5c9e8e9cbc596
canabeast <canabeast@yandex.ru>
2016-07-08 14:10:10 +0300

    added solutions for lesson 3 Part 10

---
+++ src/lesson003/Part009PermutationNulls.java
@@ -0,0 +1,23 @@
+package lesson003;
+
+public class Part009PermutationNulls {
+	public static void main(String[] args) {
+		int[] myArray = { 45, 42, 0, 5, 54, 0, 5, 0, 41, 7 };
+		int[] myNewArray = new int[10];
+		int i = 0, n = 0;
+		for (; i < 10; i++) {
+			if (myArray[i] != 0) {
+				myNewArray[n] = myArray[i];
+				n++;
+			}
+			System.out.printf("%4d",myArray[i]);
+		}
+		
+		System.out.println();
+		
+		myArray = myNewArray;
+		myNewArray = null;
+		
+		for (int value : myArray)System.out.printf("%4d",value);
+	}
+}
---
+++ src/lesson003/Part010MatrixDiagonals.java
@@ -0,0 +1,50 @@
+package lesson003;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Part010MatrixDiagonals {
+
+	public static void main(String[] args) {
+		Random random = new Random();
+		int n = 1, x, y;
+		String str1 = new String();
+		String str2 = new String();
+		int[][] myArray;
+		while (true) {
+			Scanner scEnterNum = new Scanner(System.in);
+			System.out.print("Введите размер матрицы n (не меньше 4х): ");
+			if (scEnterNum.hasNextInt()) {
+				n = scEnterNum.nextInt();
+				if (n < 4) {
+					System.out.println("Заданное число меньше 4");
+					continue;
+				}
+				break;
+			} else
+				System.out.println("Не корректный размер. Попробуйте еще раз.");
+		}
+
+		myArray = new int[n][n];
+		System.out.println("генерируем матрицу размером " + n + " на " + n);
+		for (x = 0; x < n; x++) {
+			for (y = 0; y < n; y++) {
+				myArray[x][y] = random.nextInt(50) - 25;
+				System.out.printf("%4d", myArray[x][y]);
+				if (x == y) {
+					str1 = str1.concat(Integer.toString(myArray[x][y]));
+					str1 = str1.concat(", ");
+				}
+
+				if (y + x == n - 1) {
+					str2 = str2.concat(Integer.toString(myArray[x][y]));
+					str2 = str2.concat(", ");
+				}
+			}
+			System.out.println();
+		}
+		System.out.println("Главная диагональ: " + str1);
+		System.out.println("Побочная диагональ: " + str2);
+	}
+
+}
